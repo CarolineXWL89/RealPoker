9ffbcead1f1aa05b9beb903edc81591307d9fe11
Sam <samclark24xc@gmail.com>
2017-11-11 22:27:15 -0800

    Sam # The Star Wars Episode where Luke loses his hand.
    Checks too see the highest hand, returning as an array of ints the hierarcy.
    The straight loops may not work.
    Hierarchy is returned in order (TYPE, HIGHCARD, SUIT), unless in case of 2 pair, where  the order is (TYPE, HIGHCARD1, HIGHCARD2,SUIT)
    The Hand class gets the "hand" of a player--aka the best of the 21 possibilities. Then, a player's hand can be compared.

--- app/src/main/java/com/example/caroline/realpoker/Hand.java
+++ app/src/main/java/com/example/caroline/realpoker/Hand.java
@@ -17,24 +17,43 @@ public class Hand {
         fullhand.addAll(centerCards);
     }
 
-    public ArrayList<Card> getBestHand(ArrayList<Card> cards){
+    public ArrayList<Integer> getBestHand(ArrayList<Card> cards){
         ArrayList<Integer> best = new ArrayList<>();
         for(int i=0;i<7;i++){
             for(int j = i+1;j<7;j++){
-                if(true){
                     ArrayList<Card> googleDocsSucks = new ArrayList<>();
                     googleDocsSucks.addAll(cards);
                     googleDocsSucks.remove(i);
                     googleDocsSucks.remove(j);
-                    compare.getHandType(googleDocsSucks);
-                    getHigherHand()
-                }
+                    ArrayList<Integer> samsPuns=compare.getHandType(googleDocsSucks);
+                    best = getHigherHand(samsPuns, best);
+
             }
         }
+        return best;
     }
 
-    public ArrayList<Integer> getHigherHand(){
+    public ArrayList<Integer> getHigherHand(ArrayList<Integer> one, ArrayList<Integer> two){
+        if((one.get(0)>two.get(0))){
+            return one;
+        }
+        else if(one.get(0)==two.get(0)){
+            for(int i = 1; i <one.size();i++){
+                if(one.get(i)>two.get(i)){
+                    return one;
+                }
+                else if(one.get(i)<two.get(i)){
+                    return two;
+                }
+                else{
 
+                }
+            }
+            return one;
+        }
+        else{
+            return two;
+        }
     }
 
 
