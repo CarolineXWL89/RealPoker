60b9065d24a505ccb2cc8fcd56423c8f0c878a8d
NicPorcu <nicolo.porcu@gmail.com>
2017-11-11 19:00:34 -0800

    Nicolo 1 images

--- app/src/main/java/com/example/caroline/realpoker/PlayPoker.java
+++ app/src/main/java/com/example/caroline/realpoker/PlayPoker.java
@@ -66,7 +66,7 @@ public class PlayPoker extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }//
 
         return super.onOptionsItemSelected(item);
     }
