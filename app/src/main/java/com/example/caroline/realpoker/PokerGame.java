1c6a40b0fb8d6d475fe185e92ba2b3e63c6c8bae
maylisw <Maylisw31@gmail.com>
2017-11-12 00:13:50 -0800

    Maylis #11

--- app/src/main/java/com/example/caroline/realpoker/PokerGame.java
+++ app/src/main/java/com/example/caroline/realpoker/PokerGame.java
@@ -29,6 +29,7 @@ public class PokerGame extends Fragment {
     private ArrayList<Card> deck;
     private int numOfPlayers;
     private Player[] players;
+    private TextView player1View, player2View, player3View, player4View, player5View, player6View;
 
     private Card myCard1, myCard2, tableCard1, tableCard2, tableCard3, tableCard4, tableCard5;
     private ImageView myCard1View, myCard2View, tableCard1View, tableCard2View, tableCard3View, tableCard4View, tableCard5View;
@@ -50,6 +51,7 @@ public class PokerGame extends Fragment {
         createCardsOnTheTable();
         createCards();
         wireWidgets();
+
 
 
         //get any other initial set up done
@@ -101,7 +103,7 @@ public class PokerGame extends Fragment {
     private void createPlayers() {
         players = new Player[numOfPlayers];
         for(int i = 0; i< numOfPlayers; i++){
-            players[0] = new Player("Player"+i,10000, getHand());
+            players[i] = new Player("Player"+i,10000, getHand());
         }
     }
 
@@ -118,23 +120,42 @@ public class PokerGame extends Fragment {
 
         tableCard1View = (ImageView) rootView.findViewById(R.id.table_card_1);
         tableCard1View.setContentDescription(tableCard1.getCardNumber()+ " of " + tableCard1.getSuitName());
-        showCard(tableCard1);
 
         tableCard2View = (ImageView) rootView.findViewById(R.id.table_card_2);
         tableCard2View.setContentDescription(tableCard2.getCardNumber()+ " of " + tableCard2.getSuitName());
-        showCard(tableCard2);
 
         tableCard3View = (ImageView) rootView.findViewById(R.id.table_card_3);
         tableCard3View.setContentDescription(tableCard3.getCardNumber()+ " of " + tableCard3.getSuitName());
-        showCard(tableCard3);
 
         tableCard4View = (ImageView) rootView.findViewById(R.id.table_card_4);
         tableCard4View.setContentDescription(tableCard4.getCardNumber()+ " of " + tableCard4.getSuitName());
-        showCard(tableCard4);
 
         tableCard5View = (ImageView) rootView.findViewById(R.id.table_card_5);
         tableCard5View.setContentDescription(tableCard5.getCardNumber()+ " of " + tableCard5.getSuitName());
-        showCard(tableCard5);
+
+        player1View = (TextView) rootView.findViewById(R.id.player_1);
+        String p1 = players[1].getName() + ": $" + players[1].getMonnies();
+        player1View.setText(p1);
+
+        player2View = (TextView) rootView.findViewById(R.id.player_2);
+        String p2 = players[2].getName() + ": $" + players[2].getMonnies();
+        player2View.setText(p2);
+
+        player3View = (TextView) rootView.findViewById(R.id.player_3);
+        String p3 = players[3].getName() + ": $" + players[3].getMonnies();
+        player3View.setText(p3);
+
+        player4View = (TextView) rootView.findViewById(R.id.player_4);
+        String p4 = players[4].getName() + ": $" + players[4].getMonnies();
+        player4View.setText(p4);
+
+        player5View = (TextView) rootView.findViewById(R.id.player_5);
+        String p5 = players[5].getName() + ": $" + players[5].getMonnies();
+        player5View.setText(p5);
+
+        player6View = (TextView) rootView.findViewById(R.id.user);
+        String p6 = players[0].getName() + ": $" + players[0].getMonnies();
+        player6View.setText(p6);
     }
 
     private void showCard(Card myCard) {
@@ -154,7 +175,7 @@ public class PokerGame extends Fragment {
         } else if(myCard.equals(tableCard5)){
             tableCard5View.setImageResource(res);
         }
-    }//
+    }
 
     public ArrayList<Card> getHand() {
         int cardPlace = (int)( Math.random()*deck.size());
